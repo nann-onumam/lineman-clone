0f6fa6b896f5e63f6cf9a5da714172d0c488c12c
nann-onumam <nann-onumam+noreply@users.noreply.github.com>
2026-03-03 19:18:39 +0700

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,5 +47,92 @@ module.exports = {
         ],
       },
     },
+    {
+      files: ['src/features/**/components/**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'typeAlias',
+            format: ['PascalCase'],
+            custom: {
+              regex: 'Props$',
+              match: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['src/features/**/screens/**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'function',
+            modifiers: ['exported'],
+            format: ['PascalCase'],
+            custom: {
+              regex: 'Screen$',
+              match: true,
+            },
+          },
+          {
+            selector: 'typeAlias',
+            format: ['PascalCase'],
+            custom: {
+              regex: 'Props$',
+              match: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['src/features/**/hooks/**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'function',
+            modifiers: ['exported'],
+            format: ['camelCase'],
+            custom: {
+              regex: '^use[A-Z]',
+              match: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['src/core/models/**/*.{ts,tsx}'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'interface',
+            format: ['PascalCase'],
+            custom: {
+              regex: '(Item|Model)$',
+              match: true,
+            },
+          },
+        ],
+      },
+    },
+    {
+      files: ['src/shared/constants/**/*.{ts,tsx,js,jsx}'],
+      rules: {
+        '@typescript-eslint/naming-convention': [
+          'error',
+          {
+            selector: 'variable',
+            modifiers: ['const'],
+            format: ['UPPER_CASE'],
+          },
+        ],
+      },
+    },
   ],
 };
